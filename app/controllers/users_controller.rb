7a58fe80d36edb6895d98ca709a52e3555ee3eb2
murakumo <murakumo2000@gmail.com>
2017-10-18 10:20:57 +0000

    recommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :grade, :basecost, :unitprice1, :unitprice2, :unitprice3, :unitprice4)
+      params.require(:user).permit(:name, :password, :password_confirmation, :grade, :basecost, :unitprice1, :unitprice2, :unitprice3, :unitprice4)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :articles
   resources :users
+  #get 'users/show/:name' => 'users#show'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
