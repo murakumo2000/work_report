730f03667b8e8dfecf351634a8f2abfc1dee0fca
murakumo <murakumo2000@gmail.com>
2017-10-19 11:08:58 +0000

    recommit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name].downcase)
     if user && user.authenticate(params[:session][:password])
-      reset_session
-      session[:user_id] = user.id
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid name/password combination'
       render 'new'
     end
+  end
+  
+  def destroy
+    log_out
+    redirect_to login_url
   end
 end
